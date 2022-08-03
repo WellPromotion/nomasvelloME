7bc750e9e6a5d18ccf13377f9a53db8e985a1326
almin-m <almin.muharemovic@gmail.com>
2022-08-03 15:13:41 +0200

    b

---
+++ pages/test.jsx
@@ -0,0 +1,43 @@
+import Head from 'next/head'
+import { gql, useQuery } from '@apollo/client'
+import GET_TEST from '../Querries/test'
+import client from '../Components/AppolloClient'
+// import GET_MSG from '../Querries/message'
+
+
+export default function Test(props) {
+    
+    const { forms } = props
+    console.log(forms)
+
+  return (
+
+    <>
+      
+      <div>
+          <p>
+             
+          </p>
+
+      </div>
+
+     
+     
+     
+          
+      
+      </> 
+     
+   
+  )
+}
+
+export async function getStaticProps() {
+    const result = await client.query( { query: GET_TEST } )
+      return{
+        props: {
+          forms: result.data
+        },
+        revalidate: 10,
+      }
+  }
