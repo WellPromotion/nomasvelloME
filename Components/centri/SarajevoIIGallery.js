fbbf95b644f54e7fe2f40192cec370e290e4b7b0
Cbelica <124574358+Cbelica@users.noreply.github.com>
2023-04-25 19:46:10 +0200

    Update SarajevoIIGallery.js

--- Components/centri/SarajevoIIGallery.js
+++ Components/centri/SarajevoIIGallery.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import Carousel from 'react-grid-carousel'
-import viber_image_2023 from '../../public/images/centri/sarajevo-ii/viber_image_2023-04-07_13-05-41-554.jpg'
 import sarajevo1 from '../../public/images/centri/sarajevo-ii/starigrad_centar_01.jpg'
 import sarajevo2 from '../../public/images/centri/sarajevo-ii/starigrad_centar_02.jpg'
 import sarajevo3 from '../../public/images/centri/sarajevo-ii/starigrad_centar_03.jpg'
 import sarajevo4 from '../../public/images/centri/sarajevo-ii/starigrad_centar_04.jpg'
 import sarajevo5 from '../../public/images/centri/sarajevo-ii/starigrad_centar_05.jpg'
 import sarajevo6 from '../../public/images/centri/sarajevo-ii/starigrad_centar_06.jpg'
-import sarajevo6 from '../../public/images/centri/sarajevo-ii/starigrad7.jpg'
+import sarajevo7 from '../../public/images/centri/sarajevo-ii/viber_image_2023-04-07_13-05-41-554.jpg'
 
 
 
