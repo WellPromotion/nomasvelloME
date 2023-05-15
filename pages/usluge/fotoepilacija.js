0203e3070cf65ca26df6ef19bea08f4da9cb85ee
Cbelica <124574358+Cbelica@users.noreply.github.com>
2023-05-15 20:20:21 +0200

    Update fotoepilacija.js

--- pages/usluge/fotoepilacija.js
+++ pages/usluge/fotoepilacija.js
@@ -207,7 +207,7 @@ svjetlost
                             <div className="inner">
                                 <img src={priceTag} className="priceTag" alt="55 KM - Cijena laserske epilacije po zoni" />
                                 <p className="priceTitle">
-                                49KM Zona
+                                55KM Zona
                                 </p>
                                 <p className="priceSubtitle">
                                 Unisex tarifa
@@ -216,7 +216,7 @@ svjetlost
                         </div>
                     </div>
                     <div className="column diodniCol">
-                        <img className='diodniIMG' src={priceTwo} alt="55 KM - Cijena laserske epilacije po zoni" />
+                        <img className='Naljepnica krug fotoepilacija 55 KM malaIMG' src={priceTwo} alt="55 KM - Cijena laserske epilacije po zoni" />
                     </div>
                     </div>
                     
