c23baf8889a0d2bcf9040d456723748681f3421a
WellPromotion <almin.muharemovic@gmail.com>
2025-07-25 17:11:29 +0200

    new images dobrinja, and centered images on single center slider

--- Components/centri/SarajevoIIIGallery.js
+++ Components/centri/SarajevoIIIGallery.js
@@ -4,6 +4,8 @@ import sarajevo1 from '../../public/images/centri/sarajevo-iii/novigrad_centar_0
 import sarajevo2 from '../../public/images/centri/sarajevo-iii/novigrad_centar_02.jpg'
 import sarajevo3 from '../../public/images/centri/sarajevo-iii/novigrad_centar_03.jpg'
 import sarajevo4 from '../../public/images/centri/sarajevo-iii/novigrad_centar_04.jpg'
+import sarajevoa from '../../public/images/centri/sarajevo-iii/100-a.jpg'
+import sarajevob from '../../public/images/centri/sarajevo-iii/101-b.jpg'
 
 
 // your custom dot component with prop `isActive`
@@ -24,7 +26,21 @@ export const SarajevoIIIGallery = () => {
     return (
       
 
-    <Carousel dot={MyDot1} cols={1} rows={1} gap={10} loop showDots>
+      <Carousel dot={MyDot1} cols={1} rows={1} gap={10} loop showDots>
+         <Carousel.Item>
+                <div className="centarSlide">
+                <a href="">
+                <img className="sliderCentarImage" src={sarajevoa} alt="" />
+                </a>
+            </div>
+        </Carousel.Item>
+        <Carousel.Item>
+                <div className="centarSlide">
+                <a href="">
+                <img className="sliderCentarImage" src={sarajevob} alt="" />
+                </a>
+            </div>
+        </Carousel.Item>
         <Carousel.Item>
             <div className="centarSlide">
                 <a href="">
@@ -39,20 +55,7 @@ export const SarajevoIIIGallery = () => {
                 </a>
             </div>
         </Carousel.Item>
-        <Carousel.Item>
-                <div className="centarSlide">
-                <a href="">
-                <img className="sliderCentarImage" src={sarajevo3} alt="" />
-                </a>
-            </div>
-        </Carousel.Item>
-        <Carousel.Item>
-                <div className="centarSlide">
-                <a href="">
-                <img className="sliderCentarImage" src={sarajevo4} alt="" />
-                </a>
-            </div>
-        </Carousel.Item>
+       
         {/* ... */}
     </Carousel>
 
