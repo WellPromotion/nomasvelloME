ac4c752ec005be22186a237a9a6c7b2986e67f6a
WellPromotion <almin.muharemovic@gmail.com>
2025-12-05 20:30:22 +0100

    .s

--- Components/centri/PodgoricaIIGallery.js
+++ Components/centri/PodgoricaIIGallery.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Carousel from 'react-grid-carousel'
 import brcko1 from '../../public/images/centri/podgoricaII/podgorica1.jpg'
 import brcko2 from '../../public/images/centri/podgoricaII/podgorica2.jpg'
-
+ 
  
 
 // your custom dot component with prop `isActive`
