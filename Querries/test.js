da2386dc0ea2898881d198d3e91a0f1bc4cdc363
WellPromotion <almin.muharemovic@gmail.com>
2026-06-12 17:39:34 +0200

    blog querry change

--- Querries/test.js
+++ Querries/test.js
@@ -1,25 +1,24 @@
 import { gql } from "@apollo/client"
 
 const GET_TEST = gql`
-query GET_TEST {
-  posts(first: 999) {
-    nodes {
-      date
-      excerpt
-      slug
-      title
-      featuredImage {
-        node {
-          mediaItemUrl
-          sourceUrl(size: LARGE)
-          id
+  query GET_TEST {
+    posts(first: 999, where: { categoryIn: [5, 97] }) {
+      nodes {
+        date
+        excerpt
+        slug
+        title
+        featuredImage {
+          node {
+            mediaItemUrl
+            sourceUrl(size: LARGE)
+            id
+          }
         }
+        id
       }
-      id
     }
   }
-  }
-  
-    `
+`
 
-    export default GET_TEST
+export default GET_TEST
